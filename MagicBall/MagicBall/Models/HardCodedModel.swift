b771cc9504304bfff12859e5c6ce1e6453a8634b
ivanbiben <91324448+ivanbiben@users.noreply.github.com>
2022-01-31 20:40:18 +0200

    Create view

---
+++ MagicBall/MagicBall/Models/HardCodedModel.swift
@@ -0,0 +1,8 @@
+//
+//  HardCodedModel.swift
+//  MagicBall
+//
+//  Created by Biben on 31.01.2022.
+//
+
+import Foundation
---
+++ MagicBall/MagicBall/Models/MockModel.swift
@@ -0,0 +1,8 @@
+//
+//  MockModel.swift
+//  MagicBall
+//
+//  Created by Biben on 31.01.2022.
+//
+
+import Foundation
